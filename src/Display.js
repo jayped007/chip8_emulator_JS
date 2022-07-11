de565b3ef4c6bbfbff65dab3c6d0627c04cd47d6
Jay Pedersen <jayped007@gmail.com>
2022-07-11 06:52:06 -0500

    lec4

---
+++ src/Display.js
@@ -0,0 +1,6 @@
+export class Display {
+    constructor() {
+        console.log('Construct a new Display object');
+        this.screen = document.querySelector('canvas');
+    }
+}
---
+++ src/constants/displayConstants.js
@@ -0,0 +1,2 @@
+export const DISPLAY_WIDTH = 64; // Chip8 window width
+export const DISPLAY_HEIGHT = 32; // Chip8 window height
